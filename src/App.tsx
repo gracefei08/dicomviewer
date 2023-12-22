22166913fdb9d801419feee48fcfe804e1f3f391
Fei <mfei1225@gmail.com>
2023-12-21 18:05:59 -0800

    content script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import Button from '@mui/material/Button';
 import HeaderComp from './Components/HeaderComp';
 import Divider from '@mui/material/Divider';
+import {useChromeStorageLocal} from 'use-chrome-storage';
+
 
 function App() {
+  const [value, setValue, isPersistent, error, isInitialStateResolved] = useChromeStorageLocal('test', "init");
   return (
     <div>
       <HeaderComp></HeaderComp>
       <Divider />
       <Button variant="outlined">Outlined</Button>
+      <div>Persisted in chrome.storage.local: {value.toString()}</div>
     </div>
   );
 }
